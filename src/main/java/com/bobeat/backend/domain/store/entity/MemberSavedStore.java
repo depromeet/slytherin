5c560524f1a2eefa32fce47ad990e03e33b6602e
david-parkk <ji0513ji@naver.com>
2025-09-04 21:11:53 +0900

    [common][fix] memberStore -> memberSavedStore

--- src/main/java/com/bobeat/backend/domain/store/entity/MemberSavedStore.java
+++ src/main/java/com/bobeat/backend/domain/store/entity/MemberSavedStore.java
@@ -14,10 +14,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "member_store")
+@Table(name = "member_saved_store")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class MemberStore {
+public class MemberSavedStore {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
