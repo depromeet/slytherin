375ed0a837c8c0d88b2c7193651e67249681bcf3
david-parkk <ji0513ji@naver.com>
2025-09-04 18:20:32 +0900

    [common][feat] memberStore 엔티티 추가

---
+++ src/main/java/com/bobeat/backend/domain/store/entity/MemberStore.java
@@ -0,0 +1,33 @@
+package com.bobeat.backend.domain.store.entity;
+
+import com.bobeat.backend.domain.member.entity.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "member_store")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberStore {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
