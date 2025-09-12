ede187fccc6d2c02151eaafe6607311acffe4839
sunwon <jsw5913@naver.com>
2025-09-13 02:41:24 +0900

    Revert "[Report][Feat] 식당 제보 API 구현"
    This reverts commit 2730d40494b149499ba381c8da24869b7833a2e9.

--- src/main/java/com/bobeat/backend/domain/report/repository/StoreReportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bobeat.backend.domain.report.repository;
-
-import com.bobeat.backend.domain.report.entity.StoreReport;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StoreReportRepository extends JpaRepository<StoreReport, Long> {
-}
