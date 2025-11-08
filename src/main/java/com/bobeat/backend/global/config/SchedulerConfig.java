63a2f6549eefb663a17b26aae568d4139f5ec705
david-parkk <ji0513ji@naver.com>
2025-11-08 20:16:24 +0900

    [Common][refactor] config 분리

---
+++ src/main/java/com/bobeat/backend/global/config/SchedulerConfig.java
@@ -0,0 +1,11 @@
+package com.bobeat.backend.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@EnableJpaAuditing
+public class SchedulerConfig {
+}
