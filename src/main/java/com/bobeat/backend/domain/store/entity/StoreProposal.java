c677d1c65c470c88b70e1ff99046e362eaace276
david-parkk <ji0513ji@naver.com>
2025-11-08 15:44:50 +0900

    [Food][Feat] 식당 제안 엔티티 추가

---
+++ src/main/java/com/bobeat/backend/domain/store/entity/StoreProposal.java
@@ -0,0 +1,35 @@
+package com.bobeat.backend.domain.store.entity;
+
+import com.bobeat.backend.domain.member.entity.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "store_proposal")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class StoreProposal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private ProposalType proposalType;
+
+    @ManyToOne
+    private Member member;
+}
