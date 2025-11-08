6f3d858a1a6c2d4ec500bbb7d896d4f515c3a6a9
sunwon <jsw5913@naver.com>
2025-11-08 16:06:06 +0900

    [Member][Refactor] 닉네임 업데이트 시 빈 문자열 검사

--- src/main/java/com/bobeat/backend/domain/member/dto/request/UpdateNicknameRequest.java
+++ src/main/java/com/bobeat/backend/domain/member/dto/request/UpdateNicknameRequest.java
@@ -1,10 +1,12 @@
 package com.bobeat.backend.domain.member.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 
 @Schema(description = "닉네임 수정 요청 DTO")
 public record UpdateNicknameRequest(
         @Schema(description = "새로운 닉네임")
+        @NotBlank(message = "닉네임은 필수 입력값입니다")
         String nickname
 ) {
 }
