cf9a919ab914a7619bd10294f82a4c677f72e9bc
david-parkk <ji0513ji@naver.com>
2025-11-09 18:35:52 +0900

    [Admin][Feat] 불필요한 코드 삭제

--- src/main/java/com/bobeat/backend/domain/store/external/kakao/service/KakaoSearchService.java
+++ src/main/java/com/bobeat/backend/domain/store/external/kakao/service/KakaoSearchService.java
@@ -1,7 +1,7 @@
 package com.bobeat.backend.domain.store.external.kakao.service;
 
 import com.bobeat.backend.domain.store.external.kakao.dto.KakaoDocument;
-import com.fasterxml.jackson.databind.JsonNode;
+import com.bobeat.backend.domain.store.external.kakao.dto.KakaoDocuments;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -21,39 +21,20 @@ public class KakaoSearchService {
     }
 
     public KakaoDocument searchAddress(String query) {
-        System.out.println("kakaoApiKey = " + kakaoApiKey);
-        JsonNode response = webClient.get()
+        KakaoDocuments response = webClient.get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/v2/local/search/keyword.json")
                         .queryParam("query", query)
                         .build())
                 .header("Authorization", "KakaoAK " + kakaoApiKey)
                 .retrieve()
-                .bodyToMono(JsonNode.class)
+                .bodyToMono(com.bobeat.backend.domain.store.external.kakao.dto.KakaoDocuments.class)
                 .block();
 
-        if (response != null && response.has("documents")) {
-            JsonNode documents = response.get("documents");
-            if (documents.isArray() && documents.size() > 0) {
-                JsonNode first = documents.get(0);
-                return KakaoDocument.builder()
-                        .addressName(first.get("address_name").asText())
-                        .categoryGroupCode(first.get("category_group_code").asText())
-                        .categoryGroupName(first.get("category_group_name").asText())
-                        .categoryName(first.get("category_name").asText())
-                        .distance(first.get("distance").asText())
-                        .id((first.get("id")).asText())
-                        .phone(first.get("phone").asText())
-                        .placeName(first.get("place_name").asText())
-                        .placeUrl(first.get("place_url").asText())
-                        .roadAddressName(first.get("road_address_name").asText())
-                        .x(first.get("x").asText())
-                        .y(first.get("y").asText())
-                        .build();
-            }
+        if (response != null && response.documents() != null && !response.documents().isEmpty()) {
+            return response.documents().get(0);
         }
-
-        throw new RuntimeException("찾을 수 없습니다");
+        return null;
     }
 
 }
