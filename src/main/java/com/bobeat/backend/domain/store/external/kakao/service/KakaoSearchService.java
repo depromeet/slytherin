1557b0eec6965c1625710a1b9e03a879f05d6aef
david-parkk <ji0513ji@naver.com>
2025-11-09 18:37:42 +0900

    [Admin][Feat] 예외 추가

--- src/main/java/com/bobeat/backend/domain/store/external/kakao/service/KakaoSearchService.java
+++ src/main/java/com/bobeat/backend/domain/store/external/kakao/service/KakaoSearchService.java
@@ -1,7 +1,10 @@
 package com.bobeat.backend.domain.store.external.kakao.service;
 
+import static com.bobeat.backend.global.exception.ErrorCode.NOT_FOUND_STORE;
+
 import com.bobeat.backend.domain.store.external.kakao.dto.KakaoDocument;
 import com.bobeat.backend.domain.store.external.kakao.dto.KakaoDocuments;
+import com.bobeat.backend.global.exception.CustomException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -34,7 +37,7 @@ public class KakaoSearchService {
         if (response != null && response.documents() != null && !response.documents().isEmpty()) {
             return response.documents().get(0);
         }
-        return null;
+        throw new CustomException(NOT_FOUND_STORE);
     }
 
 }
